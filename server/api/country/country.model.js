667da9ded6cd6ec2b8b1431003b8b8eebe10fe2b
cuscamayta <u>
2015-04-11 17:20:29 -0400

    module product

--- server/api/country/country.model.js
+++ server/api/country/country.model.js
@@ -3,9 +3,27 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
-var CountrySchema = new Schema({
+
+
+var price = new Schema({
+    product: productSchema,
+    dateProductPrice: Date,
+    price: Number
+});
+
+var productType = {
     name: String,
     description: String
-});
+};
+
+
+var productSchema = new Schema({
+    productId : Objectid,
+    name: String,
+    description: String,
+    Code: string,
+    productType: productType
+})
+
 
 module.exports = mongoose.model('Country', CountrySchema);
