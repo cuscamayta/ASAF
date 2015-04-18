8dea34443d5546d129e49d1b404abe07066ce166
cuscamayta <uscamayta.carlos@gmail.com>
2015-04-18 17:12:41 -0400

    changes in user

--- client/app/services/security/userService.js
+++ client/app/services/security/userService.js
@@ -1,29 +1,29 @@
-app.service('productService', ['$http', '$q', function ($http, $q) {
+app.service('userService', ['$http', '$q', function ($http, $q) {
 
 	this.noCache = function () {
 		return '?_' + new Date().getTime();
 	}
 
-	this.getProducts = function () {
+	this.getUsers = function () {
 		var defer = $q.defer();
-		$http.get('/api/product').success(function (response) {
+		$http.get('/api/user').success(function (response) {
 			defer.resolve(response);
 		})
 		return defer.promise;
 	}
 
-	this.save = function (product) {
+	this.save = function (user) {
 		var defer = $q.defer();
-		$http.post('/api/product', product).success(function (response) {
+		$http.post('/api/user', user).success(function (response) {
 			defer.resolve(response);
 		});
 		return defer.promise;
 	}
 
-	this.edit = function (product) {
+	this.edit = function (user) {
 		var defer = $q.defer();
 
-		$http.put('/api/product/'.concat(product._id), product).success(function (response) {
+		$http.put('/api/user/'.concat(user._id), user).success(function (response) {
 			defer.resolve(response);
 		});
 		return defer.promise;
@@ -32,7 +32,7 @@ app.service('productService', ['$http', '$q', function ($http, $q) {
 	this.remove = function (id) {
 		var defer = $q.defer();
 
-		$http.delete('/api/product/'.concat(id)).success(function (response) {
+		$http.delete('/api/user/'.concat(id)).success(function (response) {
 			defer.resolve(response);
 		});
 		return defer.promise;
